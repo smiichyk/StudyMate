721480ade2bafa60bcecd736af3c495c1cd46792
Nikita Smiichyk <nikitasmiichyk@gmail.com>
2025-05-30 21:34:39 +0100

    0.0

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Привет, JavaFX!");
+        Scene scene = new Scene(label, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("StudyMate");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

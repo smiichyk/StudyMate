4fd7f06aa7a5028b5d9e62f9b9c13e411d264aea
smiichyk <https://github.com/smiichyk>
2025-06-11 21:28:08 +0100

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,35 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Привет, JavaFX!");
-        Scene scene = new Scene(label, 300, 200);
-        stage.setScene(scene);
-        stage.setTitle("StudyMate");
-        stage.show();
+    public void start(Stage primaryStage) {
+
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/Main.fxml")));
+            Scene scene = new Scene(root, Color.rgb(255, 213, 0));
+
+            String css = Objects.requireNonNull(this.getClass().getResource("css/application.css")).toExternalForm();
+            scene.getStylesheets().add(css);
+
+            primaryStage.setTitle("StudyMate");
+            primaryStage.setScene(scene);
+            primaryStage.setMaximized(true);
+
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
